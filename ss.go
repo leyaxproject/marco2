f055f252f4dc6e7736d1760009a60b3e73e4241d
leyaxproject <hsantos@leya.com>
2016-12-12 17:47:58 +0000

    Create ss.go

---
+++ ss.go
@@ -0,0 +1,8 @@
+package xstringutils
+
+func FullName(f, l string) (string, int) {
+	full := f + " " + l + " from github do marco2 package xstringutils."
+	length := len(full)
+
+	return full, length
+}
