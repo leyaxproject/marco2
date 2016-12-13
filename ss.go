9c88f3f252fcf5ce464f029461791253cd81d8f0
leyaxproject <hsantos@leya.com>
2016-12-13 16:00:24 +0000

    Update ss.go

--- ss.go
+++ ss.go
@@ -1,7 +1,7 @@
 package xstringutils
 
 func FullName(f, l string) (string, int) {
-	full := f + " " + l + " from github do marco2 package xstringutils UPDATED."
+	full := f + " " + l + " from github do marco2 package xstringutils UPDATED 2."
 	length := len(full)
 
 	return full, length
